27637041d86707bea5470c5f535835e6a3f42ba0
dang0x5f <d4n9xcv@gmail.com>
2025-02-01 20:32:06 -0500

    init

---
+++ shelves.c
@@ -0,0 +1,51 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ncurses.h>
+#include "lua.h"
+#include "lauxlib.h"
+
+static int
+win_init(lua_State* L)
+{
+    initscr();
+    noecho();
+
+    return(0);
+}
+
+static int
+read_char(lua_State* L)
+{
+    int c;
+
+    refresh();
+    c = getch();
+
+    lua_pushinteger(L,c);
+
+    return(1);
+}
+
+static int
+win_end(lua_State* L)
+{
+    endwin();
+
+    return(0);
+}
+
+static const struct luaL_Reg lib [] = {
+     { "win_init"  , win_init  }
+    ,{ "read_char" , read_char }
+    ,{ "win_end"   , win_end   }
+    ,{ NULL        , NULL      }
+};
+
+int 
+luaopen_shelves(lua_State* L)
+{
+    luaL_newlib(L,lib);
+    return(1);
+}
