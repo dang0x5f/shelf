6d18a6f69753d2ac7d047ed13d9b3459714c7cf0
dang0x5f <d4n9xcv@gmail.com>
2025-02-10 22:35:25 -0500

    reimplemented write_fields

--- shelf.c
+++ shelf.c
@@ -77,36 +77,12 @@ add_fields(lua_State* L)
                 iter = iter->next;
 
             iter->next = malloc(sizeof(field_t));
-            iter->next->name = lua-tostring(L,-1-x);
+            iter->next->name = lua_tostring(L,-1-x);
             iter->next->next = NULL;
         }
     }
-    /* headers = malloc(sizeof(struct linked_list)); */
-    /* headers->head = NULL; */
-    /* headers->size = lua_tointeger(L,-1); */
 
-    /* for(x=headers->size;x>0;x--){ */
-
-    /*     if(headers->head == NULL){ */
-
-    /*         headers->head = malloc(sizeof(struct node_t)); */
-    /*         headers->head->name = lua_tostring(L,-1-x); */
-    /*         headers->head->next = NULL; */
-
-    /*     }else{ */
-
-    /*         struct node_t* iter = headers->head; */
-    /*         while(iter->next != NULL) */
-    /*             iter = iter->next; */
-
-    /*         iter->next = malloc(sizeof(struct node_t)); */
-    /*         iter->next->name = lua_tostring(L,-1-x); */            
-    /*         iter->next->next = NULL; */
-
-    /*     } */
-    /* } */
-
-    /* write_fields(); */
+    write_fields();
 
     return(0);
 }
@@ -114,22 +90,18 @@ add_fields(lua_State* L)
 static int
 write_fields(void)
 {
-    /* int right = 2; */
-    /* struct node_t* iter = headers->head; */
-    /* while(1){ */
-
-    /*     if(iter == NULL) */
-    /*         break; */
+    int right = 2;
+    field_t* iter = header_list->head.field_node;
+    while(iter){
     
-    /*     int len = strlen(iter->name); */
-    /*     for(int i = 0; i < len; i++){ */
-    /*         mvwaddch(stdscr, 1, right++, iter->name[i] | A_UNDERLINE); */
-    /*     } */
+        int len = strlen(iter->name);
+        for(int i = 0; i < len; i++){
+            mvwaddch(stdscr, 1, right++, iter->name[i] | A_UNDERLINE);
+        }
         
-    /*     right += 10; */
-    /*     iter = iter->next; */
-
-    /* } */
+        right += 10;
+        iter = iter->next;
+    }
 
     return(0);
 }
